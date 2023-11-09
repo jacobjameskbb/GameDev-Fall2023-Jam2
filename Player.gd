90e2f13df4d908a4439860940f76a7bd4895fd6b
atomicshark1 <jacob.james.kbb@gmail.com>
2023-11-09 15:28:28 -0800

    fire

--- Player.gd
+++ Player.gd
@@ -1,11 +1,19 @@
 extends CharacterBody2D
 
-func _physics_process(_delta):
+@onready var joey = load("res://joey.tscn")
+
+var speed = 500
+
+func _physics_process(delta):
 	if Input.is_action_pressed("left"):
-		position.x -= 10
+		position.x -= speed * delta
 	if Input.is_action_pressed("right"):
-		position.x += 10
+		position.x += speed * delta
 	if Input.is_action_pressed("down"):
-		position.y += 10
+		position.y += speed * delta
 	if Input.is_action_pressed("up"):
-		position.y -= 10
+		position.y -= speed * delta
+	if Input.is_action_pressed("fire"):
+		var new_joey = joey.instantiate()
+		new_joey.position = self.position
+		get_parent().add_child(new_joey)
