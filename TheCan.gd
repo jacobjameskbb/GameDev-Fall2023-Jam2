115b709d7a0f28e8b4af9cf83e419c6ea0e096b2
JoeJames00 <107498118+sirpineapplehead@users.noreply.github.com>
2023-12-07 15:45:31 -0800

    fixed a few things

--- TheCan.gd
+++ TheCan.gd
@@ -10,14 +10,15 @@ func _on_area_2d_body_entered(body):
 
 func _on_area_2d_2_body_entered(body):
 	if body.is_in_group('player'):
+		get_parent().level_speed = 0
 		_wait_for_fire_signal()
 
 func _fire():
-	var new_projectile = projectile
-	new_projectile.position = position
+	var new_projectile = projectile.instantiate()
+	add_child(new_projectile)
 	for body in $Area2D2.get_overlapping_bodies():
 		if body.is_in_group('player'):
-			new_projectile.angle_to((new_projectile.position - body.position).normalized().angle())
+			new_projectile.rotation = new_projectile.position.angle_to(body.position)
 
 func _wait_for_fire_signal():
 	var wait_timer = get_tree().create_timer(3.5)
@@ -25,5 +26,7 @@ func _wait_for_fire_signal():
 	_fire()
 
 func _process(_delta):
-	if health >= 0:
+	if health <= 0:
+		get_parent().level_speed = 2
 		queue_free()
+	position.x -= get_parent().level_speed
--- soup_projectile.gd
+++ soup_projectile.gd
@@ -1,8 +1,8 @@
-extends Node2D
+extends RigidBody2D
 
 func _on_area_2d_body_entered(body):
 	if body.is_in_group('player'):
 		body.die()
 
 func _process(delta):
-	transform.origin.x += 300 * delta
+	transform.origin.x += -400 * delta
