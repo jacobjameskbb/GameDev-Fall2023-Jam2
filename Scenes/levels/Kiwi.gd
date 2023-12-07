aac6a2f135f41c0a0b80a33707ef39d9c55c87ac
atomicshark1 <jacob.james.kbb@gmail.com>
2023-12-07 15:27:38 -0800

    kiwi ox and death

--- Scenes/levels/Kiwi.gd
+++ Scenes/levels/Kiwi.gd
@@ -21,3 +21,14 @@ func jump():
 	$AnimatedSprite2D.animation = 'kiwi'
 	look_at(player.global_position)
 	flying = true
+
+
+func _on_hitbox_body_entered(body):
+	if body.is_in_group("player"):
+		body.die()
+	if body.is_in_group("player_bullet"):
+		self.queue_free()
+
+
+func _on_hitbox_body_exited(body):
+	pass # Replace with function body.
