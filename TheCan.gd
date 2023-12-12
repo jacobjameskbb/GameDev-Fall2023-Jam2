397a902958c750739e51351672bf3e8746dc1088
JoeJames00 <107498118+sirpineapplehead@users.noreply.github.com>
2023-12-12 09:35:42 -0800

    The can now works

--- TheCan.gd
+++ TheCan.gd
@@ -3,14 +3,15 @@ extends Node2D
 @onready var projectile = preload("res://soup_projectile.tscn")
 
 var health = 100
+var fighting_player = false
 
 func _on_area_2d_body_entered(body):
-	if body.is_in_group('player_bullet'):
+	if body.is_in_group('player_bullet') and fighting_player:
 		health -= 1
 
 func _on_area_2d_2_body_entered(body):
 	if body.is_in_group('player'):
-		get_parent().level_speed = 0
+		fighting_player = true
 		_wait_for_fire_signal()
 
 func _fire():
@@ -18,7 +19,8 @@ func _fire():
 	add_child(new_projectile)
 	for body in $Area2D2.get_overlapping_bodies():
 		if body.is_in_group('player'):
-			new_projectile.rotation = new_projectile.position.angle_to(body.position)
+			new_projectile.rotation = position.angle_to_point(body.position)
+	_wait_for_fire_signal()
 
 func _wait_for_fire_signal():
 	var wait_timer = get_tree().create_timer(3.5)
@@ -27,6 +29,5 @@ func _wait_for_fire_signal():
 
 func _process(_delta):
 	if health <= 0:
-		get_parent().level_speed = 2
 		queue_free()
 	position.x -= get_parent().level_speed
--- soup_projectile.gd
+++ soup_projectile.gd
@@ -4,5 +4,5 @@ func _on_area_2d_body_entered(body):
 	if body.is_in_group('player'):
 		body.die()
 
-func _process(delta):
-	transform.origin.x += -400 * delta
+func _physics_process(delta):
+	position += transform.x * 10
